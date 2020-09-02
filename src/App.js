a02da6b2ef2a6d34df1841a945f181d19b73b1da
Didem Yanardağ <didem.yanardag@ismer.com.tr>
2020-09-02 18:03:36 +0300

    react js map formating

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import User from './components/User'
+import Users from './components/Users'
 import './App.css';
 
 class App extends Component {
   state = {
-    user: [
+    users: [
      {
         id:1, 
         name: "didemmm",
-        salary: 5000,
+        salary: "5000",
         department: "ARGE"
     },
     {
       id: 2, 
       name: "dilek",
-      salary: 4000,
+      salary: "4000",
       department: "pazarlama"
     },
     {
       id: 3, 
       name: "dilmen",
-      salary: 8000,
+      salary: "8000",
       department: "satış"
     },
     ]
@@ -32,7 +32,7 @@ class App extends Component {
         <div  className="container">
           <Navbar />
           <hr/>
-          <User/>
+          <Users users={this.state.users}/>
         </div>
       );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import User from './User'
 
 export default class Users extends Component {
     render() {
+        const {users} = this.props
+
         return (
             <div>
+                {
+                    users.map(user => {
+                        return(
+                            <User
+                                key= {user.id}
+                                name= {user.name}
+                                salary= {user.salary}
+                                department={user.department}
+                            />
+                        )
+                    })
                 
+                }
             </div>
         )
     }
